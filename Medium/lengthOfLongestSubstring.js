0eb5e108db2252a2fa110893d97c71adad24284a
clive <clivetoniato1@hotmail.com>
2025-09-14 14:07:10 +0100

    another medium done lengthofLongestSubstring

--- Medium/lengthOfLongestSubstring.js
+++ Medium/lengthOfLongestSubstring.js
@@ -1,5 +1,8 @@
 // https://leetcode.com/problems/longest-substring-without-repeating-characters/
-
+// Topics 
+// Hash Table 
+// String 
+// Sliding Window
 
 
 /**
@@ -7,5 +10,26 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-    
+    const letters = new Set()
+    let left = 0
+    let size = 0
+    for (let i = 0; i < s.length; i++) {
+        if(letters.has(s[i])){
+            
+        
+            for (left;letters.has(s[i])  ; left++) { 
+                letters.delete(s[left])
+                
+            }
+                        letters.add(s[i])
+
+        }
+        size = Math.max(size,letters.size)
+          letters.add(s[i])
+        
+    }
+
+    return size
 };
+
+console.log(lengthOfLongestSubstring("abcabcbb"),3)
