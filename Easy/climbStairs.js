f74a1417b68ff421770d0a0e4de9064ad1646968
clive <clivetoniato1@hotmail.com>
2025-09-15 20:32:05 +0100

    Easy climbStairs done

--- Easy/climbStairs.js
+++ Easy/climbStairs.js
@@ -3,19 +3,37 @@
 // Math 
 // Dynamic Programming
 // Memoization
-var climbStairs = function(n,step=0,obj={val:0}) {
-    // console.log(obj)
-    // wrong
-    if(n<=2) return
-    if(step>n) return;
-    if (step==n){
-        obj.val = obj.val+1;
-        return;
+// var climbStairs = function(n) {
+//  let a = 0
+//  let b = 1
+//  for (let i = 0; i < n; i++) {
+//     [a,b] = [b,a+b]    
+//  }
+//  return b
+// };
+
+var climbStairs = function(n,obj={}) {
+    if(!n) {
+        obj[n] = 1
+        return 1;
     }
-    climbStairs(n,step+=1,obj)
-    climbStairs(n,step+=2,obj)
-    
-    return obj.val
+    if(n==1) {
+        obj[n] = n
+        return 1;
+    }
+    if(!obj[n-2] ){
+        climbStairs(n-2,obj)
+    } 
+     if(!obj[n-1]){
+        climbStairs(n-1,obj)
+    } 
+    obj[n] = obj[n-2]+  obj[n-1]
+  
+
+ return obj[n] 
 };
 
-console.log(climbStairs(3),3)
+// console.log(climbStairs(1))
+// console.log(climbStairs(44))
+console.log(climbStairs(4),5)
+// console.log(climbStairs(3),3)
