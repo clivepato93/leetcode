eb022c8d6d4bd353a8dae91f871d18989bf195d6
clive <clivetoniato1@hotmail.com>
2025-09-17 19:15:11 +0100

    restarting leetcode progress

--- Easy/mergeTwoLists.js
+++ Easy/mergeTwoLists.js
@@ -1,19 +1,54 @@
-// https://leetcode.com/problems/merge-two-sorted-lists/?envType=problem-list-v2&envId=linked-list
-// Topics 
+// https://leetcode.com/problems/merge-two-sorted-lists/
 // Linked List
 // Recursion
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
 /**
  * @param {ListNode} list1
  * @param {ListNode} list2
  * @return {ListNode}
  */
+
 function ListNode(val, next) {
-    this.val = (val===undefined ? 0 : val)
-    this.next = (next===undefined ? null : next)
+  this.val = val === undefined ? 0 : val;
+  this.next = next === undefined ? null : next;
 }
 
-var mergeTwoLists = function(list1, list2) {
-    if(!list1) return list2
-    if(!list2) return list2
-    return list1.val>list2.val? new ListNode(list2.val,mergeTwoLists(list1,list2.next)):new ListNode(list1.val,mergeTwoLists(list1.next,list2))
+var mergeTwoLists = function (list1, list2) {
+  return !list1?list2: !list2 ? list1:
+  list1.val == undefined 
+    ? list2
+    : list2.val == undefined 
+    ? list1
+    : list2.val > list1.val
+    ? new ListNode(list1.val, mergeTwoLists(list1.next, list2))
+    : new ListNode(list2.val, mergeTwoLists(list1, list2.next));
 };
+
+// Iteratively to complete
+
+// const l1 = new ListNode(1, new ListNode(2, new ListNode(4)));
+
+// Second list: 1 -> 3 -> 4
+// const l2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+
+
+// console.log(
+//   mergeTwoLists(
+//     new ListNode(1, new ListNode(2, new ListNode(4))),
+//     new ListNode(2)
+//   )
+// );
+
+console.log(
+  mergeTwoLists(
+    new ListNode(1, new ListNode(2, new ListNode(4))),
+    new ListNode(1, new ListNode(3, new ListNode(4)))
+  )
+);
--- Easy/twoSum.js
+++ Easy/twoSum.js
@@ -1,8 +1,6 @@
 // https://leetcode.com/problems/two-sum/
-// Topic 
-// Array
-// Hash Table 
-
+// Array 
+// Hash Table
 /**
  * @param {number[]} nums
  * @param {number} target
@@ -10,28 +8,12 @@
  */
 
 var twoSum = function(nums, target) {
-    const obj ={}
-    for(let i=0;i<nums.length-1;i++){
-        for (let j = i+1; j < nums.length; j++) {
-            if(nums[j]+nums[i]==target){
-            return [i,j]
-        }
-            
-        }
-        
+  const table = {}
+  for(let i =0;i<nums.length;i++){
+    if(table[target-nums[i]] == undefined) table[nums[i]] = i
+    else return [table[target-nums[i]],i]
+  }  
 
-    }
 };
 
-// var twoSum = function(nums, target) {
-//     const obj ={}
-//     for(let i=0;i<nums.length;i++){
-//         if(target-nums[i] in obj){
-//             return [obj[target-nums[i]],i]
-//         }
-//         obj[nums[i]] = i
-
-//     }
-// };
-
-console.log(twoSum([2,7,11,15],9))
+console.log(twoSum([2,7,11,15],9),[0,1])
